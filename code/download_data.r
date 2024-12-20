421853890d2354edc0d219aef44779f058ec7a12
tzuhsi711 <ashanti890711@gmail.com>
2024-12-20 18:34:40 -0500

    download dataset changed and removed

--- code/download_data.r
+++ code/download_data.r
@@ -11,11 +11,3 @@ cases_raw <- request(api) |>
 
 write.csv(cases_raw, "../raw-data/COVID19_cases.csv", row.names = FALSE)
 
-# download death data and save to file
-api <- "https://data.cdc.gov/resource/3yf8-kanr.json"
-deaths_pre_2020_raw <- request(api) |>
-  req_url_query("$limit" = 10000000) |>
-  req_perform() |>
-  resp_body_json(simplifyVector = TRUE)
-
-write.csv(deaths_pre_2020_raw, "../raw-data/deaths_pre_2020.csv", row.names = FALSE)
